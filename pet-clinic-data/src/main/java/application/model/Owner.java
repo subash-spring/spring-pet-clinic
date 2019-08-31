df69308433ede0d4e93dd76543266375a060f1b0
subash <thapa.subash345@gmail.com>
2019-08-31 13:58:39 -0700

    Added visits

--- pet-clinic-data/src/main/java/application/model/Owner.java
+++ pet-clinic-data/src/main/java/application/model/Owner.java
@@ -1,10 +1,19 @@
 package application.model;
 
+import java.util.Set;
+
 /**
  * Created by subash on 8/25/19.
  */
 public class Owner extends Person {
 
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
 
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
