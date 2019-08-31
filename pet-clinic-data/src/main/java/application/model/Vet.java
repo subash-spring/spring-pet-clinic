91e04127a5f2f6b254ec8ba8232d493d31589855
subash <thapa.subash345@gmail.com>
2019-08-31 14:04:42 -0700

    added properties

--- pet-clinic-data/src/main/java/application/model/Vet.java
+++ pet-clinic-data/src/main/java/application/model/Vet.java
@@ -1,9 +1,19 @@
 package application.model;
 
+import java.util.Set;
+
 /**
  * Created by subash on 8/25/19.
  */
 public class Vet extends Person {
 
+    private Set<Speciality> specialities;
 
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
