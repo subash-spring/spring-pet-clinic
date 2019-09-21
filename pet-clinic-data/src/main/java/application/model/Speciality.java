b989b69a0b026fc1fb44a2bfc85b553078b72a79
subash <thapa.subash345@gmail.com>
2019-09-21 00:17:58 -0700

    Updated Vet

--- pet-clinic-data/src/main/java/application/model/Speciality.java
+++ pet-clinic-data/src/main/java/application/model/Speciality.java
@@ -1,10 +1,17 @@
 package application.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by subash on 8/31/19.
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/application/model/Vet.java
+++ pet-clinic-data/src/main/java/application/model/Vet.java
@@ -1,13 +1,20 @@
 package application.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by subash on 8/25/19.
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
