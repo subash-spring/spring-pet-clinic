549fae1fb484cb566464d98797068d7fece50241
subash <thapa.subash345@gmail.com>
2019-09-21 02:14:25 -0700

    Speciality SD Jpa

--- pet-clinic-data/src/main/java/application/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/application/services/map/VetMapService.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
+public class VetMapService extends AbstractMapService<Vet,Long> implements VetService {
 
     private final SpecialityService specialtyService;
 
-    public VetServiceMap(SpecialityService specialtyService) {
+    public VetMapService(SpecialityService specialtyService) {
         this.specialtyService = specialtyService;
     }
 
